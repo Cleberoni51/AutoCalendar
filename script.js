2be008e875ceac2f8f6a045c685296d17529d080
Caleb Schear <caleb.schear@gmail.com>
2025-09-30 14:35:12 -0400

    first signs of my JS working, I fully intend to fumble

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 
 //Caleb Schear
 //9-30-2025
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document. querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
