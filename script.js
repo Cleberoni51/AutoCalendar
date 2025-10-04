26cc9f4047c6bde32a8ad74044bbeef83cb9a0cc
Caleb Schear <caleb.schear@gmail.com>
2025-10-04 07:26:15 -0400

    javascripting

--- script.js
+++ script.js
@@ -10,5 +10,42 @@ const email_input = document.getElementById('email-input')
 const telephone_input = document.getElementById('telephone-input')
 
 form.addEventListener('submit', (e) => {
-    location.reload();
+    let errors = []
+    if(firstname_input) {
+        errors = getSignupFormErrors(firstname_input.value, password_input.value, email_input.value, telephone_input.value)
+    }
+    else {
+       errors = getLoginFormErrors(email_input.value, password_input.value) 
+    }
+
+    if (errors.length > 0) {
+        e.preventDefault()
+    }
+
 })
+
+function getSignupFormErrors(firstname, email, password, telephone) {
+    let errors = []
+
+    if (firstname === '' || firstname == null) {
+        errors.push('firstname is required')
+        firstname_input.parentElement.classList.add('incorrect')
+    }
+
+    if (password === '' || password == null) {
+        errors.push('password is required')
+        password_input_input.parentElement.classList.add('incorrect')
+    }
+
+    if (email === '' || email == null) {
+        errors.push('email is required')
+        email_input_input.parentElement.classList.add('incorrect')
+    }
+
+    if (telephone === '' || telephone == null) {
+        errors.push('telephone is required')
+        telephone_input_input.parentElement.classList.add('incorrect')
+    }
+
+    return errors;
+}
